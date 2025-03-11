ab92ce49ed6347df952063a8abccedce8bb390d4
eladjak <eladhiteclearning@gmail.com>
2025-03-11 16:59:55 +0200

    תיקון: הוספת אייקון chevronRight שהיה חסר

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -17,6 +17,20 @@ export const Icons = {
       <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
     </svg>
   ),
+  chevronRight: (props: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="m9 18 6-6-6-6" />
+    </svg>
+  ),
   google: (props: LucideProps) => (
     <svg role="img" viewBox="0 0 24 24" {...props}>
       <path
